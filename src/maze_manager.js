677e14ecfb22ed8d74373a741b58d4948a0853da
dainei-makino <47985031+dainei-makino@users.noreply.github.com>
2025-07-26 20:33:32 +0900

    Show meteor field only in boss room

--- src/maze_manager.js
+++ src/maze_manager.js
@@ -609,6 +609,9 @@ export default class MazeManager {
     if (chunk.restPoint) {
       info.restPoint = true;
     }
+    if (isBossRoom) {
+      info.isBossRoom = true;
+    }
     info.entranceDoorSprite = fromObj.doorSprite;
     if (fromObj.doorSprite) {
       fromObj.sprites = fromObj.sprites.filter(s => s !== fromObj.doorSprite);
